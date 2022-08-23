beabacc2f20f6896c3d498c0d1c4b4200b969bd3
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2022-08-23 18:10:02 -0400

    completed delete logic; still need to modify the size

--- cmd/todoDelete.go
+++ cmd/todoDelete.go
@@ -5,10 +5,14 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
+	"strings"
 
+	dir "github.com/ruokun-niu/calcli/constants"
 	"github.com/spf13/cobra"
 )
 
@@ -20,11 +24,16 @@ var deleteCmd = &cobra.Command{
 The deleted item will not be added to the completed list
 `,
 	Run: func(cmd *cobra.Command, args []string) {
+		//TODO add a check to see if an argument is passed
+		// maybe a fatal check?
 		toDelIndex, err := strconv.Atoi(args[0])
 		if err != nil {
 			log.Fatalf("an error has occurred when trying to delete an item, err: %d", err)
 		}
-
+		err = deleteItem(toDelIndex)
+		if err != nil {
+			log.Fatalf("an error has occurred when trying to delete an item, err: %d", err)
+		}
 		fmt.Println("delete called")
 	},
 }
@@ -41,4 +50,58 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func deleteItem(index int) error {
+	directory := dir.TodoDirectory
+
+	folderDir := "/Users/ruokunniu/calcli/foo.txt"
+
+	newFile, err := os.Create(folderDir)
+	if err != nil {
+		return err
+	}
+	defer newFile.Close()
+
+	originalFile, err := os.Open(directory)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(originalFile)
+	scanner.Scan()
+	_, err = newFile.WriteString(scanner.Text())
+	_, err = newFile.WriteString("\n")
+	for i := 0; i < index-1; i++ {
+		// Prior to reaching the item
+		scanner.Scan()
+		_, err = newFile.WriteString(scanner.Text())
+		_, err = newFile.WriteString("\n")
+	}
+	//Skipping one line
+	scanner.Scan()
+	for scanner.Scan() {
+		// Scan to the end of the file
+		// changing the indices along the way
+		newText := editIndex(scanner.Text())
+		_, err = newFile.WriteString(newText)
+		_, err = newFile.WriteString("\n")
+	}
+	newFile.Sync()
+
+	//rename foo
+	err = os.Rename(folderDir, directory)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func editIndex(currItem string) string {
+	originalStrIndex := strings.Split(currItem, " ")[0]
+	index, _ := strconv.Atoi(originalStrIndex)
+	index--
+	strIndex := strconv.Itoa(index)
+	result := strings.Replace(currItem, originalStrIndex, strIndex, 1)
+	return result
 }
