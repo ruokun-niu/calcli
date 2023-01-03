52bf82299420370d12e3c014d671e5a80a8ff8cd
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2023-01-02 20:32:06 -0800

    Enhanced the functionality of view (#17)
    * allows users to view the complete list
    
    * view will output the number of items in the first row

--- cmd/todoView.go
+++ cmd/todoView.go
@@ -27,14 +27,22 @@ If the todo list has not been initialized yet, it will prompt the user to do so`
 			fmt.Println(`Hmmm seems like the todo list is not found
 Have you run the command 'calcli todo init'?`)
 		} else {
-			index, err := ViewIndex(dir.TodoDirectory)
-			if err != nil {
-				log.Fatalf("An error has occurred, err: %d", err)
-			}
-			_ = index
-			err = viewItems()
-			if err != nil {
-				log.Fatalf("An error has occurred, err: %d", err)
+			viewComplete, _ := cmd.Flags().GetBool("complete")
+			if viewComplete { // if we are viewing the complete list
+				err := viewCompletedItems()
+				if err != nil {
+					log.Fatalf("An error has occurred, err: %d", err)
+				}
+			} else { // viewing the normal todo list
+				// index, err := ViewIndex(dir.TodoDirectory)
+				// if err != nil {
+				// 	log.Fatalf("An error has occurred, err: %d", err)
+				// }
+				// _ = index
+				err := viewItems()
+				if err != nil {
+					log.Fatalf("An error has occurred, err: %d", err)
+				}
 			}
 		}
 	},
@@ -42,7 +50,7 @@ Have you run the command 'calcli todo init'?`)
 
 func init() {
 	todoCmd.AddCommand(todoViewCmd)
-
+	todoViewCmd.Flags().BoolP("complete", "c", false, "View the complete list")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
@@ -54,6 +62,37 @@ func init() {
 	// todoViewCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func viewCompletedItems() error {
+	directory := dir.CompleteDirectory
+	file, err := os.Open(directory)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	index, err := ViewIndex(directory)
+	if err != nil {
+		return err
+	}
+	if index == 1 {
+		fmt.Printf("There is %d item in the completed list\n", index)
+	} else {
+		fmt.Printf("There are %d items in the completed list\n", index)
+	}
+
+	scanner.Scan() //Skipping the first line
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
 func viewItems() error {
 	directory := dir.TodoDirectory
 	file, err := os.Open(directory)
@@ -63,6 +102,16 @@ func viewItems() error {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	index, err := ViewIndex(directory)
+	if err != nil {
+		return err
+	}
+	if index == 1 {
+		fmt.Printf("There is %d item in the todo list\n", index)
+	} else {
+		fmt.Printf("There are %d items in the todo list\n", index)
+	}
+	scanner.Scan() //Skipping the first line
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 	}
