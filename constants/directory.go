3a8990f5a6113a8111d9db7ac09863a5258cdb6f
Ruokun (Tommy) Niu <ruokunniu@gmail.com>
2023-01-31 18:11:09 +0800

    configures directory (#19)

--- constants/directory.go
+++ constants/directory.go
@@ -1,7 +1,23 @@
 package constants
 
-const (
-	TodoDirectory       = "/Users/ruokunniu/calcli/todo.txt"
-	CompleteDirectory   = "/Users/ruokunniu/calcli/complete.txt"
-	TodoFolderDirectory = "/Users/ruokunniu/calcli/"
+import (
+	"log"
+	"os/user"
 )
+
+var currUser string = getCurrUser()
+var TodoDirectory string = "/Users/" + currUser + "/calcli/todo.txt"
+var CompleteDirectory string = "/Users/" + currUser + "/calcli/complete.txt"
+var TodoFolderDirectory string = "/Users/" + currUser + "/calcli/"
+var TodoRenameDirectory string = "/Users/" + currUser + "/calcli/foo.txt"
+var CompleteRenameDirectory string = "/Users/" + currUser + "/calcli/temp.txt"
+
+func getCurrUser() string {
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	username := user.Username
+	return username
+}
